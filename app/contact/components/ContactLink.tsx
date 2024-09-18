e32eae6cc94a2da97e16e04aab493fdc264d8d8e
jesus clemente <jcdeveloper92@gmail.com>
2024-09-18 17:39:39 -0400

    updt: many component refactors in the ContactSection

---
+++ app/contact/components/ContactLink.tsx
@@ -0,0 +1,14 @@
+import { IconProps } from "@tabler/icons-react";
+
+interface ContactLinkProps {
+  href: string;
+  icon: React.ComponentType<IconProps>; // Define el tipo del icono
+}
+
+export default function ContactLink({ href, icon: Icon }: ContactLinkProps) {
+  return (
+    <a href={href} target="_blank" className="flex items-center hover:text-yellow-300 hover:scale-125 transition-all ease-in-out">
+      <Icon size={20} />
+    </a>
+  );
+}
---
+++ app/contact/components/ProfileCard.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+interface ProfileCardProps {
+    alias: string;
+    firstName: string;
+    lastName: string;
+    birthDay: string;
+    city: string;
+    country: string;
+    gender: string;
+}
+
+export default function ProfileCard({ alias, firstName, lastName, birthDay, city, country, gender }: ProfileCardProps) {
+
+    const formatedBirthday = new Date(birthDay).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        timeZone: "UTC",
+      });
+
+    return (
+        <div className="flex flex-col md:flex-row justify-center items-center gap-10 p-10">
+        <Image
+          alt="hero"
+          src="/hero.jpg"
+          width={300}
+          height={300}
+          className="rounded-full object-cover max-w-full h-auto shadow-md shadow-primary-foreground"
+        />
+        <div className="text-center md:text-left">
+          <h1 className="text-3xl font-bold text-yellow-500 text-shadow-alias">{`${alias}`}</h1>
+          <h1 className="text-3xl font-bold text-primary-foreground">{`${firstName}`}</h1>
+          <h1 className="text-3xl font-bold text-primary-foreground">{`${lastName}`}</h1>
+          <p className="text-sm text-secondary-foreground">{`${city}, ${country} `}</p>
+          <p className="text-sm text-secondary-foreground">{`${formatedBirthday} `}</p>
+          <p className="text-sm text-secondary-foreground">{`${gender} `}</p>
+        </div>
+      </div>
+    )
+}
