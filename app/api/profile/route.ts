f38dda30e4105e375a5927421f1ff41b685847e8
jesus clemente <jcdeveloper92@gmail.com>
2024-09-28 12:20:18 -0400

    add: profile get method

---
+++ app/api/profile/route.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
+const prisma = new PrismaClient();
+
+export async function GET() {
+    // TRY AND CATCH
+    try {
+        const profile = await prisma.user.findFirst({
+            select: {
+                id: true,
+                first_name: true,
+                last_name: true,
+                alias: true,
+                email: true,
+                phone: true,
+                birth_day: true,
+                gender: true,
+                city: true,
+                country: true,
+                profile_img: true,
+                job_title: true,
+                about_me: true,
+                slogan: true,
+                about_list: true,
+                socials: true,
+                skills: true,
+                projects: true,
+                createdAt: false,
+                updatedAt: false,
+            } 
+        });
+        return NextResponse.json(profile);
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({ error: error.message }, { status: 500 });
+        }
+    }
+}
