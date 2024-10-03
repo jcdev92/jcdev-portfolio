806e299bd9335ddca7e5f176c432ea84d7d1ab79
jesus clemente <jcdeveloper92@gmail.com>
2024-10-03 11:24:03 -0400

    fix: some styles

--- app/(portfolio)/skills/SkillsSection.tsx
+++ app/(portfolio)/skills/SkillsSection.tsx
@@ -14,8 +14,8 @@ export default function SkillsSection({skills}: SkillsProps) {
   
   return (
     <section className="flex items-center justify-center h-screen">
-      <div className="relative flex flex-col h-full w-full items-center justify-center overflow-hidden rounded-lg bg-background/30 px-5">
-        <h1 className="p-10 text-center text-5xl md:text-6xl  font-extrabold  bg-clip-text text-transparent bg-gradient-to-r from-yellow-300 to-indigo-900">
+      <div className="relative flex flex-col w-full items-center justify-center overflow-hidden rounded-lg bg-background/30 px-5">
+        <h1 className="p-10 lg:p-0 text-center text-5xl md:text-6xl font-extrabold  bg-clip-text text-transparent bg-gradient-to-r from-yellow-300 to-indigo-900">
           SKILLS
         </h1>
         <IconCloud iconSlugs={slugs} />
